24e4c5a4d82a702000921db46c8dad75aa5417d6
kwong991 <kgwong991@gmail.com>
2018-08-14 19:02:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,13 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get "/name" do  
     "My name is Kayla"
-  end
+    
   get "/hometown" do
     "My hometown is East Brunswick"
-  end
+    
   get "/favorite-song"
     "My favorite song is Fire"
+    end
+    end
   end
-  end
+end
  
 
